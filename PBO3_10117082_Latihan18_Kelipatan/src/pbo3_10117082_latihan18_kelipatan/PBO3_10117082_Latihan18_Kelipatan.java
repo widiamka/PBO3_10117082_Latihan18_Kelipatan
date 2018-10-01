906a65b205123c604821006e4ad5e1c6312a6143
widiamka <43339745+widiamka@users.noreply.github.com>
2018-10-01 12:23:06 +0700

    Add files via upload

---
+++ PBO3_10117082_Latihan18_Kelipatan/src/pbo3_10117082_latihan18_kelipatan/PBO3_10117082_Latihan18_Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117082_latihan18_kelipatan;
+
+/**
+ *
+ * Nama : WIDIAMEGA K
+ * Kelas : IF3
+ * NIM  : 10117082
+ * Deskripsi program  : program ini berisi program untuk menampilkan 
+ * bilangan kelipatan kelipatan 3,5 dimulai daro 3,5 sampai 35
+ */
+public class PBO3_10117082_Latihan18_Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(double i = 3.5; i <= 35; i += 3.5){
+            System.out.println( i + " ");
+        }
+    }
+    
+}
